36be76d9446826c75038cc16c3c8339c965c9383
独行 <jsw0325@163.com>
2020-04-06 22:13:40 +0800

    基本页面配置完成

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,7 +2,7 @@ module.exports = {
   title: '独行的笔记',
   description: '我的个人网站',
   head: [ // 注入到当前页面的 HTML <head> 中的标签
-    ['link', { rel: 'icon', href: './public/favicon.ico' }], // 增加一个自定义的 favicon(网页标签的图标)
+    ['link', { rel: 'icon', href: '/img/favicon.ico' }], // 增加一个自定义的 favicon(网页标签的图标)
     // ['link', { rel: 'manifest', href: '/photo.jpg' }],
     // ['link', { rel: 'apple-touch-icon', href: '/photo.jpg' }],
   ],
@@ -13,36 +13,49 @@ module.exports = {
   },
   themeConfig: {
     nav: [ // 导航栏配置
+      { text: '前端基础', link: '/base/' },
+      { text: '框架相关', link: '/frame/' },
       // 单项 text：显示文字，link：指向链接
       // 这里的'/' 指的是 docs文件夹路径
       // [以 '/' 结尾的默认指向该路径下README.md文件]
-      { text: '主页', link: '/index/' },
       {
         text: 'Link',
         items: [
           // link: 指向链接也可以是外网链接
-          { text: '百度', link: 'https://www.baidu.com' }
+          { text: '掘金', link: 'https://juejin.im/timeline' },
+          { text: '博客', link: 'http://www.jsw0.top' }
         ]
       },
-      { text: '前端基础', link: '/accumulate/' },
-      { text: '算法题库', link: '/algorithm/' },
       { text: 'GitHub', link: 'https://github.com/WalkAlone0325' }
     ],
     sidebar: { // 侧边栏配置 
       // 打开 主页链接时生成下面这个菜单
-      '/index/': [
-        //多级菜单形式
+      '/base/': [
+        ['/base/', '前端基础'],
         {
-          // 菜单名
-          title: '消化堆',
-          // 子菜单
+          title: '基础', children: [
+            ['/base/html/html', 'html'],
+            ['/base/css/css', 'css'],
+            ['/base/js/js', 'javascript'],
+          ]
+        },
+        {
+          title: '深入', children: [
+            ['/base/h5/h5', 'html5'],
+            ['/base/css3/css3', 'css3'],
+            ['/base/es6/es6', 'es6'],
+          ]
+        }
+      ],
+      '/frame/': [
+        {
+          title: '框架相关',
           children: [
-            // ['','']=>[路径,标题]
-            // 或者写成 '路径',标题自动识别为该地址的文件中的h1标题
-            // 不以 '/' 结尾的就是指向.md文件             
-            ['/index/DigestionHeap/Digested', '消化过'], // '/FAQ/DigestionHeap/Digested.md'文件
-            ['/index/DigestionHeap/Digesting', '消化中'],
-            ['/index/DigestionHeap/DigestWill', '待消化']
+            ['/frame/vue/vue', 'Vue'],
+            ['/frame/vue/vue-router', 'VueRouter'],
+            ['/frame/vue/vuex', 'Vuex'],
+            ['/frame/react/react', 'React'],
+            ['/frame/ssr/ssr', 'SSR']
           ]
         }
       ]
--- docs/README.md
+++ docs/README.md
@@ -1,24 +1,16 @@
-# Hello VuePress!
-
-::: tip 提示
-this is a tip
-:::
-::: warning 注意
-this is a tip
-:::
-
-::: danger 警告
-this is a tip
-:::
-
-:tada: :100: :bamboo: :gift_heart: :fire:
-
-```js
-module.exports = {
-  data() {
-    return {
-      msg: 'Hello VuePress!',
-    }
-  },
-}
-```
+---
+home: true,
+heroImage: /img/auator.jpg
+heroText: 独行的笔记
+tagline: 前端开发爱好者
+actionText: Start The Journey→
+actionLink: /base/
+features:
+  - title: Base - 基础
+    details: 对前端基础知识html、css、js等的整理和总结...
+  - title: Frame - 框架
+    details: 积累和收录一些有关vue、react、ssr等框架相关的知识...
+  - title: Link - 链接
+    details: 常用链接...
+footer: MIT Licensed | Copyright © 2020.04.06-present 独行
+---
