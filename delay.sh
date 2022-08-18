c41944281a9cfb19f211258e3cf8e94d86d780bd
Nebula <jsw0325@163.com>
2022-08-18 18:43:07 +0800

    chore: update build

--- delay.sh
+++ delay.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:WalkAlone0325/note.git master:gh-pages
+git push -f git@github.com:WalkAlone0325/note.git main:gh-pages
 
 cd -
